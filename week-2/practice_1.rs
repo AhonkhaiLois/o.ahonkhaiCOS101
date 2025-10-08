c7b72f3254fd6ab38c145a600aa0e773aefca4e6
AhonkhaiLois <olosenigbuan.ahonkhai@pau.edu.ng>
2025-10-08 19:54:00 +0100

    Week_2

---
+++ week-2/practice_1.rs
@@ -0,0 +1,20 @@
+fn main() {
+	println!("Welcome to COS 101!");
+
+	println!("The course learning outcom is as follows:");
+
+	println!("1.Disingush between computer and computer proramming,/n
+		      2.Develop some technique in comouter science,/n
+		      3.Understand the different areas of study in computer science,/n
+		      4.Conversant with applications of comuter science,/n  
+		      5.Navigate the career prospects in computer science./n 
+		      6.Conversant with computer programming concept.");
+
+}
+
+
+
+
+
+
+
