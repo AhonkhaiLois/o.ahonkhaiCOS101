2e5052ba811b375770dcfe4ed2c74758150d2a9b
AhonkhaiLois <olosenigbuan.ahonkhai@pau.edu.ng>
2025-11-05 15:53:11 +0000

    week6

---
+++ week6/target/debug/deps/week6.d
@@ -0,0 +1,5 @@
+C:\Users\Lois Ahonhai\Documents\o.ahonkhaiCOS101\week6\target\debug\deps\week6.d: src\main.rs
+
+C:\Users\Lois Ahonhai\Documents\o.ahonkhaiCOS101\week6\target\debug\deps\week6.exe: src\main.rs
+
+src\main.rs:
---
+++ week6/target/debug/week6.d
@@ -0,0 +1,1 @@
+C:\Users\Lois\ Ahonhai\Documents\o.ahonkhaiCOS101\week6\target\debug\week6.exe: C:\Users\Lois\ Ahonhai\Documents\o.ahonkhaiCOS101\week6\src\main.rs
