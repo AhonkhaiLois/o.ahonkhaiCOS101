c06d61b8a21a1400858e0f12cb4fb3860734d24f
AhonkhaiLois <olosenigbuan.ahonkhai@pau.edu.ng>
2025-11-20 23:45:44 +0000

    Initializing and Accessing Element

---
+++ week8/practice7/target/debug/deps/practice7.d
@@ -0,0 +1,5 @@
+C:\Users\Lois Ahonhai\Documents\o.ahonkhaiCOS101\week8\practice7\target\debug\deps\practice7.d: src\main.rs
+
+C:\Users\Lois Ahonhai\Documents\o.ahonkhaiCOS101\week8\practice7\target\debug\deps\practice7.exe: src\main.rs
+
+src\main.rs:
---
+++ week8/practice7/target/debug/practice7.d
@@ -0,0 +1,1 @@
+C:\Users\Lois\ Ahonhai\Documents\o.ahonkhaiCOS101\week8\practice7\target\debug\practice7.exe: C:\Users\Lois\ Ahonhai\Documents\o.ahonkhaiCOS101\week8\practice7\src\main.rs
